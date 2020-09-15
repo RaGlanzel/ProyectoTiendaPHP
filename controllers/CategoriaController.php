8c68b37ec81169d55db6e527616152da13ed0e13
Glanzel Rony Ariel <raglanzel.rag@gmail.com>
2020-09-14 22:08:38 -0300

    construccion

--- controllers/CategoriaController.php
+++ controllers/CategoriaController.php
@@ -4,9 +4,18 @@ require_once 'models/categoria.php';
 class categoriaController{
     public function index()
     {
+        Utils::isAdmin();
         $categoria = new Categoria();
         $categorias = $categoria->getAll();
 
        require_once 'views/categoria/index.php';
+    }
+    public function crear(){
+        Utils::isAdmin();
+        require_once 'views/categoria/crear.php';
+    }
+    public function save(){
+        Utils::isAdmin();
+        
     }
 }
--- views/categoria/index.php
+++ views/categoria/index.php
@@ -1,14 +1,17 @@
 <h1>Gestionar categorías</h1>
 
-<table border="1">
+<a href="<?=base_url?>categoria/crear" class="button button-small">
+Crear Categoria
+</a>
+<table>
     <tr>
         <th>ID</th>
         <th>Nombre</th>
     </tr>
 <?php while($cat = $categorias->fetch_object()): ?>
     <tr>
-        <td><?=$cat->id;?></td>
+        <td><?=$cat->Id;?></td>
         <td><?=$cat->nombre;?></td>
     </tr>
-<?php endwhile;?>
+<?php endwhile; ?>
 </table>
